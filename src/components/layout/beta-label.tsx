ebdfc36432cf202358d55dc4655135f39ee306b6
Yaroslav <63865083+dnrovs@users.noreply.github.com>
2025-11-30 02:31:21 +0200

    fix: beta label

--- src/components/layout/beta-label.tsx
+++ src/components/layout/beta-label.tsx
@@ -1,10 +1,4 @@
-import * as fs from 'node:fs'
 import Link from 'next/link'
-import path from 'node:path'
-
-const build = fs
-    .readFileSync(path.join(process.cwd(), '.next', 'BUILD_ID'), 'utf8')
-    .slice(0, 8)
 
 export default function BetaLabel() {
     return (
@@ -13,7 +7,7 @@ export default function BetaLabel() {
                 'bg-background/50 text-muted-foreground fixed top-1 left-1/2 z-50 -translate-x-1/2 text-xs'
             }
         >
-            beta build {build} by{' '}
+            beta build by{' '}
             <Link
                 href={'https://github.com/dnrovs'}
                 className={'text-foreground'}
