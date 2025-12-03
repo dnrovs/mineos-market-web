7da6c31b5f40f052faf05ce821290184b54b7ba0
Yaroslav <63865083+dnrovs@users.noreply.github.com>
2025-12-03 02:18:33 +0200

    style: header styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 import { Button } from '@/components/ui/shadcn/button'
 import { ChevronLeft } from 'lucide-react'
 import { useExtracted } from 'next-intl'
+import { cn } from '@/utils/shadcn'
 
-export default function Header() {
+export default function Header({
+    className,
+    ...props
+}: React.ComponentProps<'div'>) {
     const t = useExtracted()
 
     return (
-        <header className="top bg-background/75 sticky top-0 z-10 flex items-center justify-between p-3 backdrop-blur-md">
+        <header
+            className={cn(
+                'top bg-background/75 sticky top-0 z-10 flex items-center justify-between p-3 backdrop-blur-md',
+                className
+            )}
+            {...props}
+        >
             <Button variant={'ghost'} onClick={() => history.back()}>
                 <ChevronLeft />
                 {t('Back')}
