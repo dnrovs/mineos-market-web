caa41aa579f5d6be44a5fab52fb2fa469ed4cbaa
Yaroslav <63865083+dnrovs@users.noreply.github.com>
2025-12-17 16:23:33 +0200

    truncate commit message

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -78,7 +78,7 @@ export default function Overview() {
             >
                 <Card
                     className={
-                        'h-5/8 max-h-150 min-h-100 w-full cursor-default flex-col items-center justify-between border-none bg-white/50 backdrop-blur-3xl select-none max-sm:rounded-none max-sm:border-x-0 sm:h-9/12 sm:w-md dark:bg-[#24242480]'
+                        'h-5/8 max-h-150 min-h-100 w-full cursor-default flex-col items-center justify-between truncate border-none bg-white/50 p-6 backdrop-blur-3xl select-none max-sm:rounded-none max-sm:border-x-0 sm:h-9/12 sm:w-md dark:bg-[#24242480]'
                     }
                 >
                     <FlatIcon
@@ -125,13 +125,13 @@ export default function Overview() {
                         target={'_blank'}
                         ref={githubLabelRef}
                         className={
-                            'text-muted-foreground hover:text-foreground flex w-full justify-center gap-1 text-xs transition'
+                            'text-muted-foreground hover:text-foreground flex w-full justify-center gap-1 truncate text-xs transition'
                         }
                     >
                         <Github className={'size-4 fill-current'} />
 
                         {isGitDataPresent && !githubLabelHovering ? (
-                            <span>
+                            <span className={'truncate'}>
                                 {t.rich(
                                     'Build <b>{hash}</b> by {author}: {message}',
                                     {
