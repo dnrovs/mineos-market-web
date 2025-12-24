099b3594b23279b1243f9f3619f49662ef9621ae
Yaroslav <63865083+dnrovs@users.noreply.github.com>
2025-12-24 20:08:03 +0200

    redirect to login if user is not authenticated in messages page

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { useMarket } from '@/context/MarketProvider'
 import { useExtracted } from 'next-intl'
+import { useRouter } from 'next/navigation'
 
 export default function Messages() {
+    const { user } = useMarket()
+    const router = useRouter()
+
     const t = useExtracted()
+
+    if (!user) return router.push('/login')
 
     return (
         <span
