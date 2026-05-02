d1b10d60ddf178269a6813b8ff36c1d84ac3deeb
Yaroslav <63865083+dnrovs@users.noreply.github.com>
2026-05-02 15:49:25 +0300

    update ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import { defineConfig } from 'eslint/config'
 
-const eslintConfig = defineConfig([...nextVitals])
+const eslintConfig = defineConfig(
+    {
+        ignores: [
+            'src/components/shadcn',
+            'src/utils/shadcn',
+            'src/components/ui/shadcn',
+            'src/lib/shadcn',
+            'src/hooks/shadcn',
+
+            'src/components/ui/shadcn-io'
+        ]
+    },
+    [...nextVitals]
+)
 
 export default eslintConfig
